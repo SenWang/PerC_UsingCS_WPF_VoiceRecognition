f28ac421d6b1e56e4d8e0eabe1efca48efcc4615
Sen Wang <moli.wang@gmail.com>
2013-07-03 17:22:07 +0800

    init

---
+++ PerC_UsingCS_WPF/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PerC_UsingCS_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public static MainWindow mainwin;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Unloaded += MainWindow_Unloaded;
+        }
+
+        MyPipeline pipeline;
+        void MainWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (pipeline != null)
+                pipeline.Dispose();
+            
+        }
+        async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainwin = this;
+            await PipelineSetupAsync();            
+        }
+
+        Task<bool> PipelineSetupAsync()
+        {
+            return Task.Run(() => PipelineSetup());
+        }
+
+        bool PipelineSetup()
+        {
+            pipeline = new MyPipeline();
+            pipeline.LoopFrames();
+            return true;
+        }
+
+        public void ProcessDepthImage(BitmapSource source)
+        {
+            //Debug.WriteLine("深度影像 :" + source);
+            Dispatcher.BeginInvoke(
+                (Action)delegate()
+                {
+                    depthImage.Source = source;
+                }
+            );
+        }
+        public void ProcessColorImage(BitmapSource source)
+        {
+            //Debug.WriteLine("彩色影像 :" + source);
+            Dispatcher.BeginInvoke(
+            (Action)delegate()
+            {
+                colorImage.Source = source;
+            }
+            );           
+        }
+        public void SetVoiceRegcognitionResult(string content)
+        {
+            Debug.WriteLine("語音辨識結果 :" + content);
+            Dispatcher.BeginInvoke(
+            (Action)delegate()
+            {
+                Title = content;
+            }
+            );  
+        }
+    }
+}
---
+++ PerC_UsingCS_WPF/MyPipeline.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PerC_UsingCS_WPF
+{
+    class MyPipeline : UtilMPipeline
+    {
+        public MyPipeline()
+            : base()
+        {
+            EnableVoiceRecognition();
+            EnableImage(PXCMImage.ColorFormat.COLOR_FORMAT_RGB32);
+            EnableImage(PXCMImage.ColorFormat.COLOR_FORMAT_DEPTH);
+        }
+
+        public override void OnRecognized(ref PXCMVoiceRecognition.Recognition data)
+        {
+            String content = "您剛剛說的是 : " + data.dictation + ",可靠度為 : " + data.confidence; 
+            MainWindow.mainwin.SetVoiceRegcognitionResult(content);
+            Response(data.dictation.Trim().ToLower());
+        }
+
+        void Response(string content)
+        {
+            if (content == "how are you")
+                Speak("我很好");
+            else if (content == "hello")
+                Speak("哈囉");
+
+        }
+        void Speak(string response)
+        {
+            Type type = Type.GetTypeFromProgID("SAPI.SpVoice");
+            dynamic synthesizer = Activator.CreateInstance(type);
+
+            synthesizer.Rate = 0;
+            synthesizer.Volume = 100;
+            synthesizer.Speak(response);
+        }
+        public override void OnImage(PXCMImage image)
+        {
+            //Debug.WriteLine("收到影像,格式 : " + image.imageInfo.format);
+            PXCMSession session = QuerySession();
+            Bitmap bitmapimage; 
+            image.QueryBitmap(session, out bitmapimage);
+            BitmapSource source = ToWpfBitmap(bitmapimage);
+            if (image.imageInfo.format == PXCMImage.ColorFormat.COLOR_FORMAT_DEPTH)
+                MainWindow.mainwin.ProcessDepthImage(source);
+            else if (image.imageInfo.format == PXCMImage.ColorFormat.COLOR_FORMAT_RGB24)
+                MainWindow.mainwin.ProcessColorImage(source);
+
+            //base.OnImage(image);
+        }
+
+
+        public static BitmapSource ToWpfBitmap(Bitmap bitmap)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Bmp);
+
+                stream.Position = 0;
+                BitmapImage result = new BitmapImage();
+                result.BeginInit();
+                // According to MSDN, "The default OnDemand cache option retains access to the stream until the image is needed."
+                // Force the bitmap to load right now so we can dispose the stream.
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();
+                return result;
+            }
+        }
+    }
+}
